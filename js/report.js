addafd47f8e992f58f0ac6458ce3af94f8e0bf66
Hiroki Takano <white_hero_0204@yahoo.co.jp>
2016-03-24 01:51:11 +0900

    workshop reports page added

---
+++ js/report.js
@@ -0,0 +1,58 @@
+var db_report = [
+  {id: '001', date: '2011.04.03', title: '大学って何のためにあるの？', url: 'http://blog.livedoor.jp/midwhite/archives/2900440.html', doc: 'http://blog.livedoor.jp/midwhite/archives/2900440.html'},
+  {id: '002', date: '2011.05.01', title: '経済学的視点を身につける', url: 'http://blog.livedoor.jp/midwhite/archives/3172978.html', doc: 'http://blog.livedoor.jp/midwhite/archives/3172978.html'},
+  {id: '003', date: '2011.05.08', title: '水資源を巡る環境問題とビジネスアプローチ', url: '', doc: 'http://blog.livedoor.jp/midwhite/archives/3262597.html'},
+  {id: '004', date: '2011.06.05', title: '食品偽装問題からCSRを考える', url: 'http://blog.livedoor.jp/midwhite/archives/3326244.html', doc: ''},
+  {id: '005', date: '2011.07.10', title: '哲学思想史', url: '', doc: 'http://blog.livedoor.jp/midwhite/archives/3886989.html'},
+  {id: '006', date: '2011.08.11', title: '学校制度は本当に必要か', url: '', doc: 'http://blog.livedoor.jp/midwhite/archives/4135058.html'},
+  {id: '007', date: '2011.09.04', title: '経済リテラシーワークショップ', url: 'http://blog.livedoor.jp/midwhite/archives/4228807.html', doc: ''},
+  {id: '008', date: '2011.11.19', title: '思想としての経済学：資本主義とグローバリズム', url: 'http://blog.livedoor.jp/midwhite/archives/4839809.html', doc: ''},
+  {id: '009', date: '2011.12.18', title: 'TPPについて考える', url: 'http://blog.livedoor.jp/midwhite/archives/4839871.html', doc: ''},
+  {id: '010', date: '2012.02.20', title: '実践的哲学：議論の名手になるには', url: 'http://blog.livedoor.jp/midwhite/archives/5160500.html', doc: 'http://blog.livedoor.jp/midwhite/archives/5222605.html'},
+  {id: '011', date: '2012.02.26', title: '資本主義とは何か：人間の幸福を確保する社会システム論', url: 'http://blog.livedoor.jp/midwhite/archives/5163268.html', doc: 'http://blog.livedoor.jp/midwhite/archives/5277139.html'},
+  {id: '012', date: '2012.04.07', title: '教養とは何か：大学教養課程の意義を考える', url: 'http://blog.livedoor.jp/midwhite/archives/5344820.html', doc: ''},
+  {id: '013', date: '2012.05.13', title: '教育格差：学校にできること、できないこと', url: 'http://blog.livedoor.jp/midwhite/archives/5461477.html', doc: ''},
+  {id: '014', date: '2012.05.27', title: '第1回 哲学総選挙：西洋哲学vs東洋哲学', url: 'http://blog.livedoor.jp/midwhite/archives/5461850.html', doc: ''},
+  {id: '015', date: '2012.08.26', title: 'ジャーナリズムと情報操作：プロパガンダがもたらす利益と流血', url: 'http://blog.livedoor.jp/midwhite/archives/5822774.html', doc: ''},
+  {id: '016', date: '2012.09.02', title: '現象学と半実在思考：議論をリードする哲学', url: 'http://blog.livedoor.jp/midwhite/archives/5825120.html', doc: ''},
+  {id: '017', date: '2012.09.22', title: '学校制度は必要か', url: '', doc: ''},
+  {id: '018', date: '2012.09.30', title: '第2回 哲学総選挙：西洋哲学vs東洋哲学', url: '', doc: ''},
+  {id: '019', date: '2013.01.13', title: '義務教育課程における私立校の役割', url: '', doc: ''},
+  {id: '020', date: '2013.11.24', title: '20年後の日本を考える', url: '', doc: ''},
+  {id: '021', date: '2014.04.29', title: '「学ぶ」と「働く」を接続する：僕らにとって「豊かな学び」とは', url: 'http://blog.livedoor.jp/midwhite/archives/7212476.html', doc: 'http://blog.livedoor.jp/midwhite/archives/7234440.html'},
+  {id: '022', date: '2014.08.16', title: '僕らの政治参加の方法', url: 'http://blog.livedoor.jp/midwhite/archives/7422683.html', doc: ''},
+  {id: '023', date: '2014.09.14', title: '僕らはグローバル化の先にどんな社会を選択するのか', url: '', doc: 'http://blog.livedoor.jp/midwhite/archives/7542668.html'},
+  {id: '024', date: '2014.09.23', title: '日本は美しい国か', url: 'http://blog.livedoor.jp/midwhite/archives/7496984.html', doc: 'http://blog.livedoor.jp/midwhite/archives/7542702.html'},
+  {id: '025', date: '2014.12.13', title: 'ソーシャルビジネスと公共性', url: '', doc: 'http://blog.livedoor.jp/midwhite/archives/7685787.html'},
+  {id: '026', date: '2014.12.31', title: '年の瀬に、この国の未来を考える。', url: '', doc: ''},
+  {id: '027', date: '2015.02.07', title: '女性の社会進出を考える', url: '', doc: 'http://blog.livedoor.jp/midwhite/archives/7792743.html'},
+  {id: '028', date: '2015.04.18', title: '第1回 学問フェスタ', url: 'http://blog.livedoor.jp/midwhite/archives/7864344.html', doc: ''},
+  {id: '029', date: '201', title: '教育と多様性・メディアについて考える', url: '', doc: ''},
+  {id: '030', date: '2015.05.30', title: 'グローバル時代の言語教育を考える：言語を操るとはどういうことか？', url: '', doc: 'http://blog.livedoor.jp/midwhite/archives/7966419.html'},
+  {id: '031', date: '2015.06.20', title: '松下村塾に学ぶ：現代の私塾の役割と可能性', url: '', doc: 'http://blog.livedoor.jp/midwhite/archives/8182327.html'},
+  {id: '032', date: '2015.07.04', title: '21世紀の働き方を考える', url: '', doc: ''},
+  {id: '033', date: '2015.09.19', title: '本当の教育経済学を語ろう：社会問題の分析視点としての経済学', url: '', doc: 'https://docs.google.com/presentation/d/1TL-8SQwccK4rLI6W_ZnPv5DeWWmA3OG4Ws5FKHzq8Bk/pub?start=false&loop=true&delayms=3000&slide=id.p'},
+  {id: '034', date: '2015.09.26', title: '歴史教育への批判を現役歴史教師にぶつける会', url: '', doc: ''},
+  {id: '035', date: '2015.10.31', title: '教育機会の多様性について語ろう', url: '', doc: ''},
+  {id: '036', date: '2015.11.07-08', title: '学園祭出張ワークショップ「Seven Problems」', url: 'http://www.cloud-academy.org/lp/kiin/', doc: ''},
+  {id: '037', date: '2015.11.14', title: '日本の格差をガッツリ経済学で捉える会', url: '', doc: 'https://docs.google.com/presentation/d/199XeZvgqoB4dfdQTatCp9CJC_taod6sCx5sx1hJnhbo/edit'},
+  {id: '038', date: '2016.04.10', title: '第2回 学問フェスタ', url: '', doc: ''}
+];
+
+
+$(function(){
+  for(var i = 0; i < db_report.length; i++){
+    var html_report = '';
+    html_report += '<li>';
+    html_report += '  <p class="number">Cloud.A Workshop #'+db_report[i]['id']+'</p>';
+    html_report += '  <h2><a href="'+db_report[i]['url']+'" target="_blank">'+db_report[i]['title']+'</a></h2>';
+    html_report += '  <p class="date">'+db_report[i]['date']+'</p>';
+    if(db_report[i]['doc']){
+      html_report += '  <a href="'+db_report[i]['url']+'" title="Cloud.A Workshop「'+db_report[i]['title']+'」" target="_blank" class="detail-btn">資料</a>';
+    }
+
+    html_report += '</li>';
+
+    $('.reports-list').prepend(html_report);
+  }
+});
